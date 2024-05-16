0760863f4d816725a27dee4d68dc792d1f9ae32f
arumami88 <kousuke.imamura@gmail.com>
2024-05-16 17:12:16 +0900

    Update bluetoothbutton.py

--- bluetoothbutton.py
+++ bluetoothbutton.py
@@ -5,32 +5,33 @@ if __name__ == '__main__':
 
     print("Waiting for device to become ready...")
     dev_path = ""
-    while(True):
+    while True:
         devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
         for device in devices:
-            if("Shutter" in device.name):
+            if "ATUMTEK" in device.name:
                 dev_path = device.path
-        if (dev_path == "") :
-            sleep(1)
-        else : break
+        if dev_path == "":
+            time.sleep(1)
+        else:
+            break
 
-	device = evdev.InputDevice(dev_path)
+    dev = evdev.InputDevice(dev_path)
     print("IoT Button is ready.")
+    start = 0
     while True:
-    	try:
-    		print(device)
-    		print("OUTPUT:")
-    		for event in device.read_loop():
-    			if event.type == evdev.ecodes.EV_KEY:
-    				if event.value == 1:
-    					start = time.time()
-    					print(" push:")
-    				if event.value == 0:
-    					end = time.time()
-    					t = end - start
-    					print(" release:", t)
-
-    	except KeyboardInterrupt:
-        	break
+        try:
+            print(dev)
+            print("OUTPUT:")
+            for event in dev.read_loop():
+                if event.type == evdev.ecodes.EV_KEY:
+                    if event.value == 1:
+                        start = time.time()
+                        print(" push:")
+                    if event.value == 0:
+                        end = time.time()
+                        t = end - start
+                        print(" release:", t)
+        except KeyboardInterrupt:
+            break
 
 print("\n")
