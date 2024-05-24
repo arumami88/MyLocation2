d2ff1479638b281c87c72795ca2aa1701e225817
arumami88 <kousuke.imamura@gmail.com>
2024-05-24 09:44:58 +0900

    Update chrometest.py

--- chrometest.py
+++ chrometest.py
@@ -3,9 +3,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 options = Options()
-options.add_argument(“--kiosk”)
-options.experimental_option("excludeSwitches", ['enable-automation'])
-options.binary_location = ("/usr/bin/chromium-browser")
-service = Service("/usr/bin/chromedriver")
+options.add_argument('--kiosk')
+options.experimental_option('excludeSwitches', ['enable-automation'])
+options.binary_location = ('/usr/bin/chromium-browser')
+service = Service('/usr/bin/chromedriver')
 driver = webdriver.Chrome(options=options, service=service)
 driver.get('https://google.com')
