15852cb14cc3001ca141a00244b78559577d735d
arumami88 <kousuke.imamura@gmail.com>
2024-05-24 09:37:42 +0900

    Create buttontest.py

---
+++ buttontest.py
@@ -0,0 +1,7 @@
+import evdev
+
+dev = evdev.InputDevice('/dev/input/event6')
+for event in device.read_loop():
+	if event.type == evdev.ecodes.EV_KEY:
+		if event.value == 0:
+			print(event.code, evdev.ecodes.KEY[event.code])
