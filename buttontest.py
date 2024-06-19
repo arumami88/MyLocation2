d83c988b5570cb14ea8bf6cbcf21e44edde75556
arumami88 <kousuke.imamura@gmail.com>
2024-06-19 09:24:44 +0900

    Delete buttontest.py

--- buttontest.py
+++
@@ -1,7 +0,0 @@
-import evdev
-
-device = evdev.InputDevice('/dev/input/event0')
-for event in device.read_loop():
-	if event.type == evdev.ecodes.EV_KEY:
-		if event.value == 0:
-			print(event.code, evdev.ecodes.KEY[event.code])
